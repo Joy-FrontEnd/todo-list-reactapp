dadd0e6e5baee5ab88eef8b612d7514509960e48
Joy-FrontEnd <bmmuthonijoy@gmail.com>
2021-01-31 17:00:15 +0300

    updated the style when todo is completed

--- src/App.js
+++ src/App.js
@@ -11,10 +11,30 @@ class App extends React.Component{
         this.state = { 
         todos: todosData
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(id){
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id){
+                   return{
+                       ...todo,
+                       completed: !todo.completed
+                   }
+                }
+                return todo
+            })
+            return {
+                todos : updatedTodos
+           }
+        })
+            //console.log("Changed", id)
     }
 
     render(){
-        const todoItem = this.state.todos.map((item) => <TodoItems key={item.id} item = {item} />)
+        const todoItem = this.state.todos.map((item) => <TodoItems key={item.id} item = {item}
+         handleChange={this.handleChange}/>)
+        
         return (
         <div className="todo-list">
        {todoItem}
--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 function TodoItems(props){
+  const styleItem ={
+    fontStyle : "italic",
+    textDecoration : "line-through",
+  }
   return (
     <div className= "todo-items">
-       <input type = "checkbox" checked={props.item.completed} />
-        <p>{props.item.text}</p>
+       <input 
+       type = "checkbox"
+        checked={props.item.completed} 
+        onChange={() => props.handleChange(props.item.id)} />
+        <p style = {props.item.completed ? styleItem : null}>{props.item.text}</p>
     </div>
   )
 }
